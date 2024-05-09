9ab35671ebe59de2a81caeb49c18805cb2007f6c
Furkan Ünsalan <furkanunsalan@gmail.com>
2024-05-10 01:26:37 +0300

    .

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -7,7 +7,6 @@ export function Gallery() {
     return (
         <div className="grid grid-cols-2 gap-4 md:grid-cols-4 bg-gradient-to-bl from-gray-900 to-gray-800">
             <div className="grid gap-4">
-                <Image path={p1} alt="blabla" date="today" location="istanbul"/>
                 <Image path={p2} alt="blabla" date="today" location="istanbul"/>
             </div>
             <div className="grid gap-4">
