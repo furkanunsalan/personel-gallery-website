c92ca272e134888d226fe1e952337f252166194f
Furkan Ünsalan <me@furkanunsalan.dev>
2024-07-25 13:06:47 +0300

    testing api

--- api/ping.js
+++ api/ping.js
@@ -1,3 +1,8 @@
-module.exports = (req, res) => {
-    res.status(200).json({ message: "this is furkan's gallery website"});
+// api/ping.js
+export default (req, res) => {
+    try {
+        res.status(200).json({ message: "furkan's gallery website" });
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 };
